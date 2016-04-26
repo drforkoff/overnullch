f41aed0612f790dda72141731403cbe5c9e19f75
Juribiyan <snivy@ponyasha.ru>
2016-04-26 22:39:58 +0500

    New domain

--- util/overnullch-links.user.js
+++ util/overnullch-links.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Overnullch Links
-// @namespace    http://0chan.cf
-// @version      0.1
-// @description  Synchronises links on 0chan.cf
+// @namespace    http://0chan.one
+// @version      0.1.1
+// @description  Synchronises links on 0chan.one
 // @author       Juribiyan
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
 if(window.self !== window.top && typeof parent.frames['list'] !== 'undefined' && document.location.pathname !== '/frame.html' && document.location.pathname !== '/index.html') {
-	parent.frames['list'].window.postMessage(document.location.href, "http://0chan.cf");
+	parent.frames['list'].window.postMessage(document.location.href, "http://0chan.one");
 }
